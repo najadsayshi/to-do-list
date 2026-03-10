2a0c39396054a1b16bfd92477811f5a1f909c7a5
Najad <mrnajadas@gmail.com>
2026-03-10 18:33:26 +0000

    feat(todo): implement CRUD operations and project cleanup
    - add create todo endpoint
    - add get todos endpoint for authenticated user
    - add update todo endpoint with partial updates (PATCH)
    - add delete todo endpoint
    - enforce owner authorization for todo updates
    - improve update logic using model_dump(exclude_unset=True)
    - add .gitignore for venv, .env, and __pycache__
    - remove cached Python files from repository

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import  HTTPAuthorizationCredentials, HTTPBearer
 from sqlmodel import SQLModel,Session, select
 from db import engine
-from auth import create_token
-from models import User,UserCreate, UserRead, UserLogin
+from auth import create_token, verify_token
+from models import User,UserCreate, UserRead, UserLogin, Todo,TodoCreate,TodoRead, TodoUpdate
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message":"Hello world"}
+
+#cors for frontend to access the api from different origin
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 @app.on_event("startup")
 async def on_startup():
@@ -17,7 +33,7 @@ def create_session():
     with Session(engine) as session:
         yield session
 
-@app.post("/signup",response_model=UserRead)
+@app.post("/signup",response_model=UserRead, status_code=201)
 async def signup(user : UserCreate,db:Session =  Depends(create_session)):
     email = user.email.lower().strip()
 
@@ -53,7 +69,119 @@ async def login(user: UserLogin, db : Session = Depends(create_session)):
     access_token = create_token(db_user.id,db_user.name)
     
     return {
-        "access_token ": access_token
+        "access_token": access_token
     }
 
 
+security = HTTPBearer()
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security),
+                     db : Session = Depends(create_session)
+                    ):
+    token = credentials.credentials
+
+    try:
+        payload = verify_token(token)
+
+        if payload is None:
+            raise HTTPException(stauts_code=401, detail="Invalid token")
+        
+        user_id = payload["sub"]
+
+        statement = select(User).where(User.id == user_id)
+
+        user = db.exec(statement).first()
+        return user
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid token")    
+
+@app.get("/todos" ,response_model=list[TodoRead])
+async def get_todos(
+    db : Session = Depends(create_session),
+    current_user: User = Depends(get_current_user),
+):
+    
+    statement = select(Todo).where(Todo.owner_id == current_user.id).order_by(Todo.created_at.desc())
+    todos = db.exec(statement).all()
+
+    return todos
+
+
+@app.post("/todos", response_model=TodoRead, status_code =201)
+async def create_todo(
+    todo : TodoCreate,
+    db : Session = Depends(create_session),
+    current_user: User = Depends(get_current_user)
+):
+    db_todo = Todo(
+        description=todo.description,
+        due_date=todo.due_date,
+        priority=todo.priority,
+        owner_id=current_user.id
+    )
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+
+@app.patch("/todos/{todo_id}", response_model=TodoRead)
+async def update_todo(
+    todo_id: int,
+    todo: TodoUpdate,
+    db: Session = Depends(create_session),
+    current_user: User = Depends(get_current_user)
+):
+
+    statement = select(Todo).where(Todo.id==todo_id)
+    db_todo = db.exec(statement).first()
+
+    if not db_todo:
+        raise HTTPException(status_code=404, detail= "Todo not found")
+
+    if db_todo.owner_id!=current_user.id:
+        raise HTTPException(status_code=403, detail= "No authorisation")
+    
+
+    # Convert the incoming update model into a dictionary.
+    # exclude_unset=True ensures that only fields actually sent in the request body
+    # are included. Fields the user didn't send will be ignored.
+    todo_data = todo.model_dump(exclude_unset=True)
+
+    # Loop through each field provided in the request.
+    # Example: {"description": "New task", "priority": "high"}
+    for key, value in todo_data.items():
+
+        # Dynamically update the corresponding attribute on the database object.
+        # Example:
+        # key = "description"
+        # value = "New task"
+        # becomes → db_todo.description = "New task"
+        setattr(db_todo, key, value)
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+
+@app.delete("/todos/{todo_id}")
+async def deleteTodo(
+    todo_id : int,
+    todo : TodoCreate,
+    current_user : User = Depends(get_current_user),
+    db : Session = Depends(create_session)
+):
+    
+    statement = select(Todo).where(todo_id == Todo.id)
+    db_todo = db.exec(statement).first()
+
+    if not db_todo:
+        raise HTTPException(status_code=404,detail="No such To do list")
+    
+    if db_todo.owner_id != current_user.id:
+        raise HTTPException(status_code=403, detail = "No authorization babes")
+    
+    db.delete(db_todo)
+    db.commit()
+
+    return {"message": "Todo deleted successfully"}
--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from datetime import datetime, timezone
+from typing import Optional
+from enum import Enum
 from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from pydantic import BaseModel
@@ -5,7 +9,7 @@ from pydantic import BaseModel
 class User(SQLModel, table = True):
     id : int | None = Field(default=None,primary_key=True)
     name : str
-    email : str
+    email : str = Field(index= True, unique=True)
     password : str
     
 
@@ -30,3 +34,38 @@ class Token(BaseModel):
     token_type : str
 
 
+class Priority(str,Enum):
+    low = "low"
+    medium = "medium"
+    high = "high"
+
+class Todo(SQLModel, table = True):
+    id : Optional[int] =  Field(default=None,primary_key=True)
+    description : str
+    completed : bool = Field(default=False)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    due_date : Optional[datetime] = None
+    priority : Priority = Field(default=Priority.medium)
+    owner_id : int = Field(foreign_key="user.id")
+
+
+class TodoCreate(SQLModel):
+    description : str
+    due_date : Optional[datetime] = None
+    priority : Priority = Priority.low  
+
+
+class TodoRead(SQLModel):
+    id : int
+    description : str
+    completed : bool
+    created_at : datetime
+    due_date : Optional[datetime]
+    priority : Priority
+
+
+class TodoUpdate(SQLModel):
+    description: Optional[str] = None
+    completed: Optional[bool] = None
+    due_date: Optional[datetime] = None
+    priority: Optional[Priority] = None
